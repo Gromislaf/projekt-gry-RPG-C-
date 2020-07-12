05f45c87c7411276999911be1be3217a24796974
Gromislaf <61653197+Gromislaf@users.noreply.github.com>
2020-07-12 21:48:55 +0200

    dodanie logiki aplikacji

--- space_warrior/Program.cs
+++ space_warrior/Program.cs
@@ -25,39 +25,70 @@ namespace space_warrior
             Console.WriteLine();
             Console.WriteLine("Toje aktualne statystyki: Poziom życia:{0}/{1} Doświadczenie:{2} Poziom: {3}", _player.CurrentHP, _player.MaxHP, _player.EXP, _player.Lvl);
             Console.WriteLine();
-            Console.WriteLine("Wybierz broń: \nBolter: wciśnij 1 \nMiecz Łańcucowy : wciśnij 2 \nKarabin Plazmowy: Wciśnij 3");
-            int a = int.Parse(Console.ReadLine());
-            a--;
-            Console.WriteLine(Things.Weapons[a]);
-            Console.WriteLine();
-
-            Console.WriteLine("Przeciwnik Naciera!");
-            Console.WriteLine();
-            Random generator = new Random();
-            int enemy = generator.Next(2);
-            Console.WriteLine(Things.Enemies[enemy]);
-            
             do
             {
-                int damageToEnemy = RandomGen.NumberBetween(Things.Weapons[a].MinimumDamage, Things.Weapons[a].MaximumDamage);
+                Console.WriteLine("Wybierz broń: \nBolter: wciśnij 1 \nMiecz Łańcucowy : wciśnij 2 \nKarabin Plazmowy: Wciśnij 3");
+            
+            
+                int a = int.Parse(Console.ReadLine());
+                a--;
+                Console.WriteLine(Things.Weapons[a]);
+                Console.WriteLine();
 
-                Things.Enemies[enemy].CurrentHP -= damageToEnemy;
-                
+                Console.WriteLine("Przeciwnik Naciera!");
                 Console.WriteLine();
-                Console.WriteLine("Zadałeś " +Things.Enemies[enemy].Name + " następującą liczbę obrażeń " + damageToEnemy);
-                Console.WriteLine();
+                Random generator = new Random();
+                int enemy = generator.Next(3);
                 Console.WriteLine(Things.Enemies[enemy]);
-                Console.WriteLine();
-                _player.CurrentHP = _player.CurrentHP - Things.Enemies[enemy].Attack;
-                Console.WriteLine(Things.Enemies[enemy].Name + " zadał Ci następującą liczbę obrażeń: " + Things.Enemies[enemy].Attack);
+
+                do
+                {
+                    int damageToEnemy = RandomGen.NumberBetween(Things.Weapons[a].MinimumDamage, Things.Weapons[a].MaximumDamage);
+
+                    Things.Enemies[enemy].CurrentHP -= damageToEnemy;
+
+                    Console.WriteLine();
+                    Console.WriteLine("Zadałeś " + Things.Enemies[enemy].Name + " następującą liczbę obrażeń " + damageToEnemy);
+                    Console.WriteLine();
+                    Console.WriteLine(Things.Enemies[enemy]);
+                    Console.WriteLine();
+                    if (Things.Enemies[enemy].CurrentHP > 0)
+                    {
+                        _player.CurrentHP = _player.CurrentHP - Things.Enemies[enemy].Attack;
+                        Console.WriteLine(Things.Enemies[enemy].Name + " zadał Ci następującą liczbę obrażeń: " + Things.Enemies[enemy].Attack);
+                        Console.WriteLine();
+                        Console.WriteLine("Aktualny poziom życia:{0}/{1} Doświadczenie:{2} Poziom: {3}", _player.CurrentHP, _player.MaxHP, _player.EXP, _player.Lvl);
+                    }
 
 
-            //Things.Enemies[enemy].CurrentHP = Things.Enemies[enemy].MaxHP - generator.Next(Things.Weapons[a].MinimumDamage, Things.Weapons[a].MaximumDamage + 1);
+
+                   
 
 
-            } while (Things.Enemies[enemy].CurrentHP >0 || _player.CurrentHP >0 );
+                } while (Things.Enemies[enemy].CurrentHP > 0 && _player.CurrentHP > 0);
 
-            Console.WriteLine(Things.Enemies[enemy]);
+                if (Things.Enemies[enemy].CurrentHP <= 0)
+                {
+                    Console.WriteLine("Pokonałeś wroga");
+                    Console.WriteLine();
+                    Console.WriteLine("Zdobywasz 500 pkt doświadczenia.");
+                    Console.WriteLine();
+                    _player.EXP = _player.EXP + 500;
+                    Console.WriteLine("Aktualny poziom życia:{0}/{1} Doświadczenie:{2} Poziom: {3}", _player.CurrentHP, _player.MaxHP, _player.EXP, _player.Lvl);
+                }
+                if (_player.EXP > 1600)
+                { _player.Lvl = 3; }
+                else if (_player.EXP > 800)
+                { _player.Lvl = 2; }
+
+                if (_player.Lvl == 3)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("Udało Ci się przedrzeć do reszty twojego oddziału, WYGRAŁEŚ ");
+                }
+            } while (_player.Lvl < 3);
+
+            //Console.WriteLine(Things.Enemies[enemy]);
             Console.WriteLine("Poziom życia:{0}/{1} Doświadczenie:{2} Poziom: {3}", _player.CurrentHP, _player.MaxHP, _player.EXP, _player.Lvl);
             Console.ReadKey();
             }
